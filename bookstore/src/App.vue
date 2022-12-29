92196035c4730af3d8bc56b62ed817bd96d1aec4
Wei-Ting <tony123445s54@yahoo.com.tw>
2022-12-29 13:09:47 +0800

    Initialize a Vue.js application

---
+++ bookstore/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-app :theme="theme">
+    <v-app-bar>
+      <v-spacer></v-spacer>
+
+      <v-btn
+        :prepend-icon="
+          theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'
+        "
+        @click="onClick"
+        >Toggle Theme</v-btn
+      >
+    </v-app-bar>
+
+    <v-main>
+      <v-container class="bg-surface-variant">
+        <v-row no-gutters>
+          <v-col cols="12">
+            <v-sheet class="pa-2">
+              <v-card title="Card title" text="..."></v-card>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const theme = ref('light');
+/*
+const getList(){
+
+}
+*/
+
+const onClick = () => {
+  if (theme.value === 'light') {
+    theme.value = 'dark';
+  } else {
+    theme.value = 'light';
+  }
+};
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+</style>
